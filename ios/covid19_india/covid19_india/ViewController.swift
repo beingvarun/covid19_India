02fc1d4d1fce06b6a3794363ee13bfb7e8ffaab4
Varun Valsaraj <varunvalsaraj@hotmail.com>
2020-06-22 08:22:51 +0400

    redesigned home page

---
+++ ios/covid19_india/covid19_india/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid19_india
+//
+//  Created by Varunraj on 21/06/2020.
+//  Copyright © 2020 Varunraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
